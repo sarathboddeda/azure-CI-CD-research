aec02fbf22b6ac12fac0b2f6d13b7e9cc96d6ef8
sarath <sarathboddeda@gmail.com>
2023-05-08 23:25:21 +0100

    Update settings.py

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://webdeploysarath.azurewebsites.net",
+]
+
+
 
 # Application definition
 
@@ -49,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'bookshop.urls'
